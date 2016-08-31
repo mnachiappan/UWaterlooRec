ac8912f09d6b41c8d96ff7756b83306b3bda9ee9
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2016-08-30 23:38:39 -0400

    Changed card design

---
+++ app/src/main/java/thani/labs/com/uwaterloorec/ActivityViewHolder.java
@@ -0,0 +1,19 @@
+package thani.labs.com.uwaterloorec;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by meyyappan on 2016-08-24.
+ */
+public class ActivityViewHolder extends RecyclerView.ViewHolder {
+    TextView activityName, activityLocation, activityTime;
+
+    ActivityViewHolder(View itemView) {
+        super(itemView);
+        activityName = (TextView) itemView.findViewById(R.id.activity_name);
+        activityLocation = (TextView) itemView.findViewById(R.id.activity_location);
+        activityTime = (TextView) itemView.findViewById(R.id.activity_time);
+    }
+}
